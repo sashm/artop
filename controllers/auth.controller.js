f06695f81ba9858b6fd2ff8f365e2d2e18553042
sashm <sarahshmueli@gmail.com>
2018-04-18 10:23:08 +0300

    controller file

---
+++ controllers/auth.controller.js
@@ -0,0 +1,19 @@
+﻿var express = require('express');
+var app = express();
+var router = express.Router();
+
+var mongoose = require('mongoose'); //get DB
+var connection = mongoose.createConnection('mongodb://ayalaka:artopico1234@ds040898.mlab.com:40898/artop');//connect to the db server
+var patientSchema = require('../models/patient');
+var pasiont = connection.model('patient', patientSchema);
+
+
+//var request = require('request');
+
+// routes
+router.post('/register', function (req, res) {
+    
+});
+
+module.exports = router;
+
