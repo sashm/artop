285a44218bf4e8fcd32dfbe66bb8708fb787956e
sashm <sarahshmueli@gmail.com>
2018-07-05 11:07:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var path = require('path');
 var app = express(); //init the server
 //var port = process.env.PORT || 3000;
 
-
 //initalization for using POST calls
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));//read URL encoded
@@ -28,7 +27,7 @@ app.use(express.static(path.join(__dirname,'app')));
 app.use('/app', express.static('app'));
 
 //database
-app.use('/auth', require('../controllers/auth.controller'));
+app.use('/auth', require('./controllers/auth.controller'));
 
 
 // make '/app' default route
@@ -37,6 +36,36 @@ app.get('/', function (req, res) {
 });
 
 //listen on port
+
 var server = app.listen(app.get('port'), function(){
    console.log('Server listening at http://' + server.address().address + ':' + server.address().port);
 });
+
+var router = express.Router();
+
+//var mongoose = require('mongoose'); //get DB
+//var connection = mongoose.createConnection('mongodb://ayalaka:artopico1234@ds040898.mlab.com:40898/artop');//connect to the db server
+//var patientSchema = require('../models/patient');
+//var pasiont = connection.model('patient', patientSchema);
+/*$http.get('https://api.mlab.com/api/1/databases/artop/collections/patients'
+	+'?apiKey=HUosNgRrowmswAB98uy11UXQk0OmASjo')
+	.success(function (data, status, headers, config) {
+			console.log("data",data);
+	  }).error(function (data, status, headers, config) { alert(status) });*/
+//var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope, $http) {
+    $http.get("https://api.mlab.com/api/1/databases/artop/collections/patients"
+	+"?apiKey=HUosNgRrowmswAB98uy11UXQk0OmASjo")
+    .then(function(response) {
+        $scope.myWelcome = response.data;
+        console.log("data",response);
+    });
+});
+//var request = require('request');
+
+// routes
+router.post('/register', function (req, res) {
+    
+});
+
+module.exports = router;
